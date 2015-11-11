8a27802dd401bfccef6e43482b326af49d6c2df5
Akshat Dubey <akshatdubey@outlook.com>
2015-11-12 02:12:55 +0800

    Last PS :(

--- PS6/Supermarket.java
+++ PS6/Supermarket.java
@@ -18,6 +18,8 @@ class Supermarket {
 	private int[][] memo;
 	private int INF = 1000000000;
 	private int[][] shoppingMap;
+	private int[][] distanceList;
+	private List<List<IntegerPair>> AdjList;
 
 	// if needed, declare a private data structure here that
 	// is accessible to all methods in this class
@@ -40,11 +42,21 @@ class Supermarket {
 		// then, reaches the cashier of the supermarket (back to vertex 0).
 		//
 		// write your answer here
+
+		AdjList = AdjListify(T);
+
+		distanceList = new int[K + 1][N + 1];
+		init_distList();
+		distanceList[0] = Dijkstra(0);
+		for (int i = 0; i < K; i++) {
+			distanceList[i+1] = Dijkstra(shoppingList[i]);
+		}
+
+		shoppingMap = new int[K + 1][K + 1];
+		init_shoppingMap();
+
 		memo = new int[K + 1][(int) Math.pow(2, K + 1)];
 		init_memo();
-		shoppingMap = new int[K + 1][K + 1];
-		init_floydWarshall();
-		init_shoppingMap();
 		ans = DP_TSP(0, 1);
 
 		return ans;
@@ -53,33 +65,57 @@ class Supermarket {
 	// You can add extra function if needed
 	// --------------------------------------------
 
+	void init_distList() {
+		for (int x = 0; x < K + 1; x++) {
+			int[] tempArray = new int[N + 1];
+			Arrays.fill(tempArray, INF);
+			distanceList[x] = tempArray;
+		}
+	}
+
 	void init_memo() {
 		for (int i = 0; i < K + 1; i++) {
 			Arrays.fill(memo[i], -1);
 		}
 	}
 
-	void init_floydWarshall() {
-		for (int k = 0; k < N + 1; k++) {
-			for (int i = 0; i < N + 1; i++) {
-				for (int j = 0; j < N + 1; j++) {
-					T[i][j] = Math.min(T[i][j], T[i][k] + T[k][j]);
+	int [] Dijkstra(int source) {
+		int[] dist = new int[N+1];
+		Arrays.fill(dist, INF);
+		dist[source] = 0;
+
+		PriorityQueue<IntegerPair> DJQueue = new PriorityQueue<IntegerPair>();
+		DJQueue.add(new IntegerPair(dist[source], source));
+
+		while (!DJQueue.isEmpty()) {
+			IntegerPair front = DJQueue.remove();
+			int d = front.first();
+			int u = front.second();
+			if (d == dist[u]) {
+				for (int j = 0; j < AdjList.get(u).size(); j++) {
+					IntegerPair v = AdjList.get(u).get(j);
+					if (dist[u] + v.second() < dist[v.first()]) {
+						dist[v.first()] = dist[u] + v.second();
+						DJQueue.add(new IntegerPair(dist[v.first()], v.first()));
+					}
 				}
 			}
 		}
+		return dist;
+
 	}
 
 	void init_shoppingMap() {
 		for (int i = -1; i < K; i++) {
 			for (int j = -1; j < K; j++) {
 				if (i == -1 && j != -1) {
-					shoppingMap[i + 1][j + 1] = T[0][shoppingList[j]];
+					shoppingMap[i + 1][j + 1] = distanceList[0][shoppingList[j]];
 				} else if (i != -1 && j == -1) {
-					shoppingMap[i + 1][j + 1] = T[shoppingList[i]][0];
+					shoppingMap[i + 1][j + 1] = distanceList[i+1][0];
 				} else if (i == -1 && j == -1) {
-					shoppingMap[i + 1][j + 1] = T[0][0];
+					shoppingMap[i + 1][j + 1] = distanceList[0][0];
 				} else {
-					shoppingMap[i + 1][j + 1] = T[shoppingList[i]][shoppingList[j]];
+					shoppingMap[i + 1][j + 1] = distanceList[i+1][shoppingList[j]];
 				}
 			}
 		}
@@ -87,7 +123,7 @@ class Supermarket {
 
 	int DP_TSP(int u, int mask) {
 		if (mask + 1 == 1 << K + 1) {
-			return T[u][0];
+			return shoppingMap[u][0];
 		}
 		if (memo[u][mask] != -1) {
 			return memo[u][mask];
@@ -95,11 +131,52 @@ class Supermarket {
 
 		memo[u][mask] = INF;
 		for (int v = 0; v < K + 1; v++) {
-			if (v != u && ((mask & 1 << v) == 0)) {
-				memo[u][mask] = Math.min(memo[u][mask], T[u][v] + DP_TSP(v, mask | 1 << v));
+			if ((mask & 1 << v) == 0) {
+				memo[u][mask] = Math.min(memo[u][mask], shoppingMap[u][v] + DP_TSP(v, mask | 1 << v));
 			}
 		}
 		return memo[u][mask];
+	}
+
+	// convert adjacency matrix to adj list
+	private List<List<IntegerPair>> AdjListify(int[][] Matrix) {
+		List<List<IntegerPair>> finalList = new ArrayList<List<IntegerPair>>();
+		for (int i = 0; i < N + 1; i++) {
+			List<IntegerPair> temp = new ArrayList<IntegerPair>();
+			for (int j = 0; j < N + 1; j++) {
+				temp.add(new IntegerPair(j, Matrix[i][j]));
+			}
+			finalList.add(temp);
+		}
+		return finalList;
+	}
+
+	class IntegerPair implements Comparable<IntegerPair> {
+		Integer _first, _second;
+
+		public IntegerPair(Integer f, Integer s) {
+			_first = f;
+			_second = s;
+		}
+
+		public String toString() {
+			return _first.toString() + _second.toString();
+		}
+
+		public int compareTo(IntegerPair o) {
+			if (!this.second().equals(o.second()))
+				return this.second() - o.second();
+			else
+				return this.first() - o.first();
+		}
+
+		Integer first() {
+			return _first;
+		}
+
+		Integer second() {
+			return _second;
+		}
 	}
 
 	void run() throws Exception {
